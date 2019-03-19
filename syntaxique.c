a0a371291a354c3d056d8d1ed8781f7046291ea0
Feekz <alexbenc@hotmail.fr>
2019-03-19 09:23:04 +0100

    Add files via upload

---
+++ syntaxique.c
@@ -0,0 +1,16 @@
+#include "syntaxique.h"
+
+Arbre creer_arbre (){
+	return NULL;
+}
+
+Arbre inserer( typejeton jeton, Arbre jeton_preced, Arbre jeton_suiv ) {
+
+	Arbre resultat;
+
+	resultat->valeur = valeur;
+	resultat->fils_gauche = fils_gauche;
+	resultat->fils_droit = fils_droit;
+
+	return resultat;
+}
---
+++ syntaxique.h
@@ -0,0 +1,11 @@
+#include "jeton.h"
+
+
+//Fonction de création d'un arbre vide
+Arbre creer_arbre();
+
+//Fonction de création d'un noeud
+Arbre inserer()
+
+
+
