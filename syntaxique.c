efa5ad95bbdc6b8ee69d84f7343b0b1ed1081400
Feekz <alexbenc@hotmail.fr>
2019-03-20 08:48:47 +0100

    Add files via upload

--- syntaxique.c
+++ syntaxique.c
@@ -20,6 +20,9 @@ Arbre creer_noeud( typejeton jeton, Arbre fils_gauche, Arbre fils_droit ) {
 			resultat->jeton.valeur.reel = jeton.valeur.reel;
 			break;
 
+		case VARIABLE:
+		break;
+		
 		case FONCTION:
 			resultat->jeton.valeur.fonction = jeton.valeur.fonction;
 			break;
@@ -44,10 +47,7 @@ Arbre creer_noeud( typejeton jeton, Arbre fils_gauche, Arbre fils_droit ) {
 //Gestion du cas : réel
 Arbre reel(typejeton Tab[],int i){
 
-	Arbre A;
-	A = creer_noeud(Tab[i],NULL,NULL);
-
-	return A;
+	return creer_noeud(Tab[i],NULL,NULL);
 }
 
 //------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
@@ -55,10 +55,10 @@ Arbre reel(typejeton Tab[],int i){
 Arbre operation(typejeton Tab[],Arbre A,int i){
 
 	int j = i + 1;
-	Arbre tmp;
+	//Arbre tmp;
 
-	tmp = AS(Tab, j);
-	A = creer_noeud(Tab[i], A, tmp);
+	//tmp = AS(Tab, j);
+	A = creer_noeud(Tab[i], A, creer_noeud(Tab[j],NULL,NULL));
 
 	return A;
 }
@@ -95,29 +95,28 @@ Arbre AS(typejeton Tab[], int i){
 				A = reel(Tab,i);
 				i = i + 1;
 			break;
+
 			//Si le jeton actuel est un opérateur
 			case OPERATEUR:
-				A = operation(Tab, A, i);
-				if(Tab[i+1].lexem != PAR_OUV){
-					i = i + 2;
+				if(Tab[i+1].lexem == PAR_OUV){
+					return A = creer_noeud(Tab[i],A,AS(Tab, i+1));				
 				}
 				else{
-					i = i + 1;
-				}
-				
+					A = operation(Tab, A, i);
+					i = i + 2;
+				}				
 			break;
+
 			//Si le jeton actuel est une fonction
 			/*case FONCTION:
 				A = fct(Tab, A, i);
 				i = i + 1;
 			break;*/
+			
 			//Si le jeton actuel est une parenthèse ouverte
 			case PAR_OUV:
-				A = par_ouv(Tab, A, i);
-				i = i + 1;
-			break;
-			//Si le jeton actuel est une parenthèse fermée
 			case PAR_FERM:
+				//A = par_ouv(Tab, A, i);
 				i = i + 1;
 			break;
 
@@ -141,6 +140,10 @@ void afficher( Arbre a) {
 
 		case REEL:
 			printf("%d|%f\n",a->jeton.lexem, a->jeton.valeur.reel);
+			break;
+
+		case VARIABLE:
+			printf("%d|%c\n",a->jeton.lexem, 'x');
 			break;
 
 		case FONCTION:
@@ -174,8 +177,7 @@ Arbre A;
 	
 	tab_test[0].lexem = PAR_OUV;
 
-	tab_test[1].lexem = REEL;
-	tab_test[1].valeur.reel = 1;
+	tab_test[1].lexem = VARIABLE;
 
 	tab_test[2].lexem = OPERATEUR;
 	tab_test[2].valeur.operateur = PLUS;
@@ -192,13 +194,9 @@ Arbre A;
 	tab_test[6].valeur.reel = 4;
 
 	tab_test[7].lexem = PAR_FERM;
+	tab_test[7].lexem = PAR_FERM;
 
-	tab_test[8].lexem = PAR_FERM;
-
-	tab_test[9].lexem = FIN;
-
-	//printf("%d\n",tab_test[0].lexem);
-	//printf("%f\n",tab_test[0].valeur.reel);
+	tab_test[8].lexem = FIN;
 
 A = AS(tab_test, i);
 afficher(A);
