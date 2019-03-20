829df7f756d59a08aaa59292755f527b82e1fbc9
bigcraftorman <48710941+bigcraftorman@users.noreply.github.com>
2019-03-20 10:31:44 +0100

    Update lex.c

--- lex.c
+++ lex.c
@@ -3,13 +3,13 @@
 #include<math.h>
 #include "jeton.h"
 
-
 int main() {
 
 	char expression[100]; //Expression mathématique entrée par l'utilisateur
 	int length; //longueur de l'expression
 	int i; //compteur
-	int s;
+	int imax; //nombre total d'éléments dans l'expression
+	int s; //Résultat de l'addition pour le cas des REEL
 	typejeton jeton[200]; //Sortie de l'AL
 
 printf("Rentrer la fonction ");
@@ -20,63 +20,73 @@ printf("Rentrer la fonction ");
 	length = strlen(expression);
 	printf("longueur : %d\n", length);
 
-	for (i = 0; i <= length; i++) {
+	for (i = 0; i <= length; i++) { //Detection de chaque élément de l'expression
+		
+		//Detection des lexèmes OPERATEURS 
+		
 		if (expression[i] == '+') {
-			printf("+");
+			printf("+\n");
 			jeton[i].lexem = OPERATEUR;
 			jeton[i].valeur.operateur = PLUS;
 		}
-		if (expression[i] == '-') {
+		 else if (expression[i] == '-') {
 			printf("-");
 			jeton[i].lexem = OPERATEUR;
 			jeton[i].valeur.operateur = MOINS;
 		}
-		if (expression[i] == '*') {
+		else if (expression[i] == '*') {
 			printf("*");
 			jeton[i].lexem = OPERATEUR;
 			jeton[i].valeur.operateur = FOIS;
 		}
-		if (expression[i] == '/') {
+		else if (expression[i] == '/') {
 			printf("/");
 			jeton[i].lexem = OPERATEUR;
 			jeton[i].valeur.operateur = DIV;
 		}
-		if (expression[i] == '^') {
+		else if (expression[i] == '^') {
 			printf("^");
 			jeton[i].lexem = OPERATEUR;
 			jeton[i].valeur.operateur = PUIS;
 		}
-		if (expression[i] == '(') {
+
+		//Detection des lexèmes DIVERS
+
+		else if (expression[i] == '(') {
 			printf("(");
 			jeton[i].lexem = PAR_OUV;
 		}
-		if (expression[i] == ')') {
+		else if (expression[i] == ')') {
 			printf(")");
 			jeton[i].lexem = PAR_FERM;
 		}
-		if (expression[i] == 'x') {
+		else if (expression[i] == 'x') {
 			printf("x");
 			jeton[i].lexem = VARIABLE;
 		}
-		if (expression[i] == NULL ) {
-			printf("fin");
+		else if (expression[i] == NULL ) {
+			printf("fin\n");
 			jeton[i].lexem = FIN;
 		}
-		if (expression[i] == ')') {
+		else if (expression[i] == ')') {
 			printf(")");
 			jeton[i].lexem = PAR_FERM;
 		}
-		if (expression[i]>='0' && expression[i] <= '9') {
-			int j = 0;
-			int temp[100];
-			temp[j]= (expression[i]-48);
 
-			while (expression[i] >= '0' && expression[i] <= '9') {
+		//Detection  du lexème REEL
+
+		else if (expression[i]>='0' && expression[i] <= '9') {
+			int j = 0; //compteur 
+			int temp[100]; //tableau temporaire (1 case = un chiffre)
+			temp[j]= (expression[i]-'0'); //remplissage du tableau
+
+			while (expression[i] >= '0' && expression[i] <= '9') { //Si le 
 				j++;
-				temp[j] = (expression[i] - 48);
+				temp[j] = (expression[i] - '0'); //remplissage du tableau
 				i++;
 			}
 			
+			i = i - 1; 
 			int k = j; //k est la longueur totale du nombre 
 			printf("longueur du nombre = %d\n", k);
 			j = 0;
@@ -91,7 +101,20 @@ printf("Rentrer la fonction ");
 			printf("s=%d\n", s);
 			jeton[i].lexem = REEL;
 			jeton[i].valeur.reel = s;
+			printf("jeton=%f\n", jeton[i].valeur.reel);
 		}
+		else{
+			printf("erreur\n");
+			jeton[i].lexem = ERREUR;
+		}
+	}
+
+	i = 0;
+	for (i = 0; i < length; i++) {
+		printf("lexeme = %d\n", jeton[i].lexem);
+		printf("valeur reelle = %f\n", jeton[i].valeur.reel);
+		printf("valeur operateur = %d\n", jeton[i].valeur.operateur);
+		printf("valeur fonction = %d\n", jeton[i].valeur.fonction);
 	}
 
 	_getch();
